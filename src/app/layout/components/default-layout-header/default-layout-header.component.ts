4e4ba50301c26b561a4d4a21f42f6031be1c3fd3
Lucas Silva <lucascarlosjobs@gmail.com>
2025-05-23 19:16:06 -0300

    feat(header): Implement header responsiveness and refine navigation styles

--- src/app/layout/components/default-layout-header/default-layout-header.component.ts
+++ src/app/layout/components/default-layout-header/default-layout-header.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-default-layout-header',
   templateUrl: './default-layout-header.component.html',
-  styleUrls: ['./default-layout-header.component.scss']
+  styleUrls: ['./default-layout-header.component.scss'],
 })
 export class DefaultLayoutHeaderComponent {
-
 }
